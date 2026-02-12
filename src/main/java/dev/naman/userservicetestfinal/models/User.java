f78eed438258e6af9ec90c7be3000f11e976e9c4
Raghvendra Sonarikar <raghvendrasonarikar@gmail.com>
2026-02-12 11:30:37 +0530

    Revert "Authentication Done"
    This reverts commit 142ded6595de373b4b9d6f47cd6a7b1f081c4660.

--- src/main/java/dev/naman/userservicetestfinal/models/User.java
+++ src/main/java/dev/naman/userservicetestfinal/models/User.java
@@ -1,13 +1,8 @@
 package dev.naman.userservicetestfinal.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import dev.naman.userservicetestfinal.security.CustomSpringUserDetails;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.ManyToMany;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.HashSet;
@@ -17,11 +12,9 @@ import java.util.Set;
 @Entity
 @Getter
 @Setter
-@JsonDeserialize(as = User.class)
 public class User extends BaseModel {
     private String email;
     private String password;
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JsonIgnore
+    @ManyToMany
     private Set<Role> roles = new HashSet<>();
 }
