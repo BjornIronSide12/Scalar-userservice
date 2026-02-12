28167cbf6247ad4c4831576f515d8a9b03e76c2d
Raghvendra Sonarikar <raghvendrasonarikar@gmail.com>
2026-02-12 11:29:38 +0530

    Revert "Send email on signup"
    This reverts commit ea377a7ed84a5a08ec928dfc7b93780fed471619.

--- src/main/java/dev/naman/userservicetestfinal/services/AuthService.java
+++ src/main/java/dev/naman/userservicetestfinal/services/AuthService.java
@@ -1,8 +1,5 @@
 package dev.naman.userservicetestfinal.services;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import dev.naman.userservicetestfinal.clients.KafkaProducerClient;
-import dev.naman.userservicetestfinal.dtos.SendEmailMessageDto;
 import dev.naman.userservicetestfinal.dtos.UserDto;
 import dev.naman.userservicetestfinal.models.Role;
 import dev.naman.userservicetestfinal.models.Session;
@@ -36,17 +33,11 @@ public class AuthService {
     private UserRepository userRepository;
     private SessionRepository sessionRepository;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
-    private KafkaProducerClient kafkaProducerClient;
-    private ObjectMapper objectMapper;
 
-    public AuthService(UserRepository userRepository, SessionRepository sessionRepository ,
-                       BCryptPasswordEncoder bCryptPasswordEncoder, KafkaProducerClient kafkaProducerClient,
-                       ObjectMapper objectMapper) {
+    public AuthService(UserRepository userRepository, SessionRepository sessionRepository ,BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
         this.sessionRepository = sessionRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-        this.kafkaProducerClient = kafkaProducerClient;
-        this.objectMapper = objectMapper;
     }
 
     public ResponseEntity<UserDto> login(String email, String password) {
@@ -146,21 +137,7 @@ public class AuthService {
         
         User savedUser = userRepository.save(user);
 
-        UserDto userDto = UserDto.from(savedUser);
-
-        try {
-            kafkaProducerClient.sendMessage("userSignUp", objectMapper.writeValueAsString(userDto));
-
-            SendEmailMessageDto emailMessage = new SendEmailMessageDto();
-            emailMessage.setTo(userDto.getEmail());
-            emailMessage.setFrom("admin@scaler.com");
-            emailMessage.setSubject("Welcome to Scaler");
-            emailMessage.setBody("Thanks for creating an account. We look forward to you growing. Team Scaler");
-            kafkaProducerClient.sendMessage("sendEmail", objectMapper.writeValueAsString(emailMessage));
-        } catch (Exception e) {
-            System.out.println("Something has gone wrong");
-        }
-        return userDto;
+        return UserDto.from(savedUser);
     }
 
     public SessionStatus validate(String token, Long userId) {
@@ -205,10 +182,3 @@ public class AuthService {
 // and then another call to get the details of the user
 // auth-token%3AeyJjdHkiOiJ0ZXh0L3BsYWluIiwiYWxnIjoiSFMyNTYifQ.ewogICAiZW1haWwiOiAibmFtYW5Ac2NhbGVyLmNvbSIsCiAgICJyb2xlcyI6IFsKICAgICAgIm1lbnRvciIsCiAgICAgICJ0YSIKICAgXSwKICAgImV4cGlyYXRpb25EYXRlIjogIjIzcmRPY3RvYmVyMjAyMyIKfQ.r2FVQUCn6DNHir5AlEBT2XQMgO7aN4m3xg9zcuB-zxQ
 // auth-token%3AeyJhbGciOiJIUzI1NiJ9.eyJjcmVhdGVkQXQiOjE2OTgwNzgzNDg0NTQsInJvbGVzIjpbXSwiZXhwaXJ5QXQiOjE5NjU2LCJlbWFpbCI6Im5hbWFuQHNjYWxlci5jb20ifQ._v1af8cc1YA-cEyHlX1BASwveBiASQeteWFM8UzWxfY
-
-// {
-//   to: "",
-//   from: "",
-//   subject: "",
-//   body: ""
-// }
