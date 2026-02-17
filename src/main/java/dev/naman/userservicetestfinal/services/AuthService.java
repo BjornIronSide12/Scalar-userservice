111d1f655a6601f38ad0cebfc92025686eac350e
Raghvendra Sonarikar <raghvendrasonarikar@gmail.com>
2026-02-17 11:10:13 +0530

    auth service 1

--- src/main/java/dev/naman/userservicetestfinal/services/AuthService.java
+++ src/main/java/dev/naman/userservicetestfinal/services/AuthService.java
@@ -6,26 +6,31 @@ import dev.naman.userservicetestfinal.models.SessionStatus;
 import dev.naman.userservicetestfinal.models.User;
 import dev.naman.userservicetestfinal.repositories.SessionRepository;
 import dev.naman.userservicetestfinal.repositories.UserRepository;
-import lombok.Setter;
-import org.apache.commons.lang3.RandomStringUtils;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.JwsHeader;
+import io.jsonwebtoken.security.MacAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.util.MultiValueMap;
 import org.springframework.util.MultiValueMapAdapter;
-import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import javax.crypto.SecretKey;
+import java.util.*;
 
 @Service
 public class AuthService {
     private UserRepository userRepository;
     private SessionRepository sessionRepository;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+    @Value("${jwt.expiration}")
+    private String jwtExpiration;
 
     public AuthService(UserRepository userRepository, SessionRepository sessionRepository,  BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
@@ -46,7 +51,23 @@ public class AuthService {
             throw new RuntimeException("Wrong password");
         }
 
-        String token = RandomStringUtils.randomAlphanumeric(30);
+        String token;
+
+        // Create a test key suitable for the desired HMAC-SHA algorithm:
+        MacAlgorithm alg = Jwts.SIG.HS256; //or HS384 or HS256
+        SecretKey key = alg.key().build();
+
+        Map<String, Object> jsonFroJwt = new HashMap<>();
+        jsonFroJwt.put("email",  user.getEmail());
+        jsonFroJwt.put("roles",  user.getRoles());
+        jsonFroJwt.put("createdAt", new Date());
+        jsonFroJwt.put("expiresAt", new Date(System.currentTimeMillis() + (1000 * 60 * 60)));
+
+         // Create the compact JWS:
+        token = Jwts.builder()
+                .claims(jsonFroJwt)
+                .signWith(key, alg).
+                compact();
 
         Session session = new Session();
         session.setSessionStatus(SessionStatus.ACTIVE);
@@ -72,7 +93,6 @@ public class AuthService {
 
     public ResponseEntity<Void> logout(String token, Long userId) {
         Optional<Session> sessionOptional = sessionRepository.findByTokenAndUser_Id(token, userId);
-
         if (sessionOptional.isEmpty()) {
             return null;
         }
@@ -100,8 +120,19 @@ public class AuthService {
         Optional<Session> sessionOptional = sessionRepository.findByTokenAndUser_Id(token, userId);
 
         if (sessionOptional.isEmpty()) {
-            return null;
+            return SessionStatus.ENDED;
+//            return null;
         }
+
+        Session session = sessionOptional.get();
+
+        // if the status is not active then change the status to ended and return back
+        if(!session.getSessionStatus().equals(SessionStatus.ACTIVE)) {
+            return SessionStatus.ENDED;
+        }
+
+        Jwt<JwsHeader, Claims> claimsJwt = Jwts.parser()
+                .build().parseSignedClaims(token);
 
         return SessionStatus.ACTIVE;
     }
